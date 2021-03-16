95128adbf7945c15b7b9cf78b73a590245151c3a
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-03-16 01:09:21 -0400

    Initialize the project

---
+++ src/main/java/io/ms/microservicesconfigurations/MicroservicesConfigurationsApplication.java
@@ -0,0 +1,13 @@
+package io.ms.microservicesconfigurations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesConfigurationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesConfigurationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ms/microservicesconfigurations/MicroservicesConfigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ms.microservicesconfigurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesConfigurationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
