07fdaadf90d1fb6bb1b41a16ba1987d0746f86a0
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-03-20 23:08:40 -0400

    @value annotation

--- src/main/java/io/ms/microservicesconfigurations/GreatingController.java
+++ src/main/java/io/ms/microservicesconfigurations/GreatingController.java
@@ -10,9 +10,35 @@ public class GreatingController {
 	@Value("${greating.message}")
 	private String greatingMessage;
 	
+	@Value("${greating.message1: this is the defalut message}")
+	private String defalutMessage;
+	
+	@Value("${my.list}")
+	private String myList;
+	
+	@Value("${my.dbvalues.as.object}")
+	private String mydbValuesAsObject;
 	
 	@RequestMapping("/greating")
 	public String getGreating() {
 		return greatingMessage;
+	}
+	
+	
+	@RequestMapping("/defalutValue")
+	public String getDefaultGreating() {
+		return defalutMessage;
+	}
+	
+	@RequestMapping("/mylist")
+	public String getMyList() {
+		System.out.println("my list is: " + this.myList);
+		return myList;
+	}
+	
+	@RequestMapping("/mydbvaluesasobject")
+	public String mydbValuesAsObject() {
+		System.out.println("mydbValuesAsObject: " + this.mydbValuesAsObject);
+		return mydbValuesAsObject;
 	}
 }
