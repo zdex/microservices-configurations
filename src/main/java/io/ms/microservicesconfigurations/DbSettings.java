352c319ba1154b2a96e55770c7e7f33b5fdf667b
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-03-23 13:54:55 -0400

    alignment

--- src/main/java/io/ms/microservicesconfigurations/DbSettings.java
+++ src/main/java/io/ms/microservicesconfigurations/DbSettings.java
@@ -10,24 +10,29 @@ public class DbSettings {
 	private String connection;
 	private String host;
 	private int port;
+
 	public String getConnection() {
 		return connection;
 	}
+
 	public void setConnection(String connection) {
 		this.connection = connection;
 	}
+
 	public String getHost() {
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = host;
 	}
+
 	public int getPort() {
 		return port;
 	}
+
 	public void setPort(int port) {
 		this.port = port;
 	}
-	
-	
+
 }
