07ec40e535f67dec8170d7b8d8fa9081d82fb1d0
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-03-24 17:18:30 -0400

    updated

--- src/main/java/io/ms/microservicesconfigurations/GreatingController.java
+++ src/main/java/io/ms/microservicesconfigurations/GreatingController.java
@@ -2,12 +2,14 @@ package io.ms.microservicesconfigurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreatingController {
 
 	@Value("${greating.message}")
